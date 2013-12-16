b4a3acb4656888cb13d617e381a2adeaa93bbf14
Sudeep Juvekar <sjuvekar@gmail.com>
2013-12-16 10:10:53 -0500

    more

--- Awesome/gen/com/ramsud/play/awesome/R.java
+++ Awesome/gen/com/ramsud/play/awesome/R.java
@@ -22,9 +22,13 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int r1=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int editText1=0x7f080003;
+        public static final int imageView1=0x7f080001;
+        public static final int scrollview1=0x7f080002;
+        public static final int textView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -33,9 +37,10 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int app_name=0x7f050002;
+        public static final int caption=0x7f050001;
+        public static final int hello_world=0x7f050000;
+        public static final int r1=0x7f050003;
     }
     public static final class style {
         /** 
--- Awesome/src/com/ramsud/play/awesome/MainActivity.java
+++ Awesome/src/com/ramsud/play/awesome/MainActivity.java
@@ -2,6 +2,7 @@ package com.ramsud.play.awesome;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +11,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Log.e("Banana", "Started this!");
     }
 
 
